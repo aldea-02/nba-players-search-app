26acf77f57796ab12e42cc44424182cf01c9c1ed
aldea02 <aldea02@icloud.com>
2022-10-22 21:41:55 +0300

    basic search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
+import { useState } from 'react'
 export default function Search() {
-  fetch('https://www.balldontlie.io/api/v1/players/?search=davis')
-		.then((response) => response.json())
-		.then((data) => console.log(data))
+	const [players, setPlayers] = useState([])
 
-  return <div>Search</div>;
+	async function fetchPlayers(name) {
+		const response = await fetch(`https://www.balldontlie.io/api/v1/players/?search=${name}`)
+		const players_json = await response.json()
+		setPlayers(players_json.data)
+	}
+	function refreshSearch(e) {
+		fetchPlayers(e.target.value ? e.target.value : null)
+	}
+	return (
+		<>
+			<input className='bg-slate-300' type='text' onChange={refreshSearch} />
+			<div className='item-container'>
+				{players.map((player) => (
+					<div key={player.id}>{player.first_name + ' ' + player.last_name}</div>
+				))}
+			</div>
+		</>
+	)
 }
